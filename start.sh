840f81bc1e8e91c70267a1a6cd6d2c1ed1cdb3e1
Anthony Budd <anthonybudd94@gmail.com>
2021-11-11 10:54:29 -0600

    change

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #! /bin/sh
 
-# if [ -f .env ]
-# then
-#   export $(cat .env | sed 's/#.*//g' | xargs)
-# fi
-
-# CAMERA_TIMEOUT="${CAMERA_TIMEOUT:-10}"
-
-# echo $(CAMERA_TIMEOUT)
-
 echo '   ______                                   _____       _ __      '
 echo '  / ____/___ _____ ___  ___  _________ _   / ___/____  (_) /_____ '
 echo ' / /   / __ `/ __ `__ \/ _ \/ ___/ __ `/   \__ \/ __ \/ / //_/ _ \'
@@ -18,18 +9,33 @@ echo '                                             /_/                  '
 echo ''
 echo ''
 
-echo '> Starting Web Server'
+
+# Load .env
+echo "> Loading .env"
+if [ -f .env ]
+then
+    export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+export CAMERA_TIMEOUT="${CAMERA_TIMEOUT:-10}" 
+export MEDIA_PATH="${MEDIA_PATH:-$(pwd)/images}" 
+echo "  ├── CAMERA_TIMEOUT=$CAMERA_TIMEOUT (seconds)"
+echo "  └── MEDIA_PATH=$MEDIA_PATH"
+echo ""
+
+
+# Start Web UI and Tor
+echo "> Starting Web Server"
 # docker-compose up -d
 
-echo "> Capturing Frames (every 5s)"
-echo ''
+
+# Main loop that captures images
+echo "> Capturing Frames"
+echo ""
 while :
 do
-    mkdir -p $(pwd)/images/$(date +"%Y/%m/%d")
-    
-    export IMAGE_PATH=$(pwd)/images/$(date +"%Y/%m/%d")/frame-$(date +"%H-%M-%S").jpg
-    raspistill -t 1 -o $(pwd)/images/$(date +"%Y/%m/%d")/frame-$(date +"%H-%M-%S").jpg
+    mkdir -p $MEDIA_PATH/$(date +"%Y/%m/%d")
+    export IMAGE_PATH=$MEDIA_PATH/$(date +"%Y/%m/%d")/frame-$(date +"%H-%M-%S").jpg
+    raspistill -n -t 1 -o $MEDIA_PATH/$(date +"%Y/%m/%d")/frame-$(date +"%H-%M-%S").jpg
     echo $IMAGE_PATH
-
     sleep 5
 done
